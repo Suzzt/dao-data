8fc46ad313187a704b994f62b24f387baa8ed815
sucf <1058772978@qq.com>
2022-04-10 22:34:25 +0800

    init code

--- analysis/src/main/java/com/junmo/analysis/manager/consumer/FlinkLogRecordHandler.java
+++ analysis/src/main/java/com/junmo/analysis/manager/consumer/FlinkLogRecordHandler.java
@@ -1,5 +1,13 @@
 package com.junmo.analysis.manager.consumer;
 
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+
+import java.util.Properties;
+
 /**
  * @author: sucf
  * @date: 2022/4/10 16:29
@@ -7,7 +15,17 @@ package com.junmo.analysis.manager.consumer;
  */
 
 public class FlinkLogRecordHandler {
-    public static void main(String[] args) {
+    private static final String KAFKA_CONNECT_INFO = "192.168.1.8:9092";
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+        Properties properties = new Properties();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_CONNECT_INFO);
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "first-1");
+        FlinkKafkaConsumer<String> consumer = new FlinkKafkaConsumer<>("first-test", new SimpleStringSchema(), properties);
+        DataStreamSource<String> source = env.addSource(consumer);
+        source.print();
 
+        env.execute();
     }
 }
