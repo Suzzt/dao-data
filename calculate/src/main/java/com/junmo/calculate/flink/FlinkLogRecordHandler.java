be4bc201ac4f67ea7e58bffae977cd756bd16573
sucf <1058772978@qq.com>
2022-04-13 23:29:56 +0800

    init code

--- calculate/src/main/java/com/junmo/calculate/flink/FlinkLogRecordHandler.java
+++ calculate/src/main/java/com/junmo/calculate/flink/FlinkLogRecordHandler.java
@@ -1,9 +1,15 @@
-package com.junmo.analysis.manager.consumer;
+package com.junmo.calculate.flink;
 
+import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.operators.FlatMapOperator;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.flink.util.Collector;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 
 import java.util.Properties;
@@ -13,7 +19,6 @@ import java.util.Properties;
  * @date: 2022/4/10 16:29
  * @description:
  */
-
 public class FlinkLogRecordHandler {
     private static final String KAFKA_CONNECT_INFO = "192.168.1.8:9092";
     public static void main(String[] args) throws Exception {
@@ -25,7 +30,7 @@ public class FlinkLogRecordHandler {
         FlinkKafkaConsumer<String> consumer = new FlinkKafkaConsumer<>("first-test", new SimpleStringSchema(), properties);
         DataStreamSource<String> source = env.addSource(consumer);
         source.print();
-
         env.execute();
     }
 }
+
