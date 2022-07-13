1d95011d23103b326ddf66e0e7ca18cd9730710f
sucf <sucf@shinemo.com>
2022-07-13 14:20:31 +0800

    init code

---
+++ dao-df/src/main/java/com/junmo/df/DataFactoryApplication.java
@@ -0,0 +1,16 @@
+package com.junmo.df;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: sucf
+ * @date: 2022/4/4 16:01
+ * @description:
+ */
+@SpringBootApplication
+public class DataFactoryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DataFactoryApplication.class, args);
+    }
+}
